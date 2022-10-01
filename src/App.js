5d34b1daab995acc44ac63a1f89f5edb6a9d21f7
sankesh97 <ramojitinorange@gmail.com>
2022-10-01 20:23:54 +0530

    Created App and removed boiler plate code.

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <section></section>
+        <section></section>
+        <section></section>
+        <section></section>
+        <section></section>
+      </header>
+    </div>
+  );
+}
+
+export default App;
